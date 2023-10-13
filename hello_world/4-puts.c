82b69fd1de2b6a05238352ebbf85c82d9dfcf0fe
Diesos <ozturk@live.Fr>
2023-10-13 15:43:40 +0200

    commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
+/* Puts function display a string */ 
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
+    puts( "\"Programming is like building a multilingual puzzle\n\"" );
     return 0;
 }
