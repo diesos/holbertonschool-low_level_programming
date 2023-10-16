bbc5dd4759e092a0c13b876005a9c5449763359a
Diesos <ozturk@live.Fr>
2023-10-16 11:57:22 +0200

    typo on printf

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	if (n % 10 > 0 && n % 10 < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0", n, n % 10);
 	if (n % 10 > 5)
-		printf("The last digit of %d is %d and is greater than 5", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
 	if (n == 0)
 		printf("Last digit of %d is 0 and is 0", n);
 	return (0);
