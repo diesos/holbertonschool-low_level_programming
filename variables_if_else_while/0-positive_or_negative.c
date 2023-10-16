77b99edfa200b3266a3105a4e91512c895d32beb
Diesos <ozturk@live.Fr>
2023-10-16 11:04:29 +0200

    Adding new folder for new project

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - randomize a number and assign this value to n.
+* Return: print the sign value of n
+*/
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+		printf("%d is negative", n);
+	else if (n == 0)
+		printf("0 is zero");
+	else
+		printf("%d is positive", n);
+	return (0);
+}
