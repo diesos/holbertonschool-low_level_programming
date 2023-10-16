e54ad7e6a2bd64da5baffdac0426638caad20e2c
Diesos <ozturk@live.Fr>
2023-10-16 23:27:00 +0200

    exc 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+* main - program initializing for printing char
+*
+*
+* Return: return 0
+*/
+
+int main(void)
+{
+	write(1, "_putchar\n", 10);
+	return (0);
+}
