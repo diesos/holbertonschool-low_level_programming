a7737d418584e075a85826459021847de9eb0164
Diesos <ozturk@live.Fr>
2023-10-17 17:55:06 +0200

    exc 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - computes the abs value of an int
+ * @int : integer to check the abs value of
+ * Return: Always 0.
+ */
+
+int _abs(int)
+{
+	if (int > 0)
+		printf("%d", int);
+	if (int == 0)
+		printf("0");
+	if (int < 0)
+	{
+		int = int * -int;
+		printf("%d", int);
+}
