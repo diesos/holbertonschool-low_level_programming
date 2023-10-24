50bf10101f2523527251cf6ad4a4971c3e331318
Diesos <ozturk@live.Fr>
2023-10-24 10:54:40 +0200

    formatting for betty style

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - 
- * @*n : pointer int n
+ * reset_to_98 - resetting a pointer value
+ * @n: pointer int n
  * Return: void
  */
 
- void reset_to_98(int *n)
+void reset_to_98(int *n)
 {
 	int *pt_n = n;
 	*pt_n = 98;
