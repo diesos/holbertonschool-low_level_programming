69c2f1d283b8e38ad87e6b2e5b5e9db771e2d2df
Diesos <ozturk@live.Fr>
2023-10-25 13:26:35 +0200

    exc 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* puts_half - puts half
+* @str: pointer string
+* Return: void
+*/
+
+void puts_half(char *str)
+{
+	int i;
+	int longueur;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	if (i % 2 != 0)
+		longueur = i - 1 / 2;
+	else
+		longueur = i / 2;
+	for (; longueur <= i; longueur++)
+		putchar(str[longueur]);
+}
+
+
+
