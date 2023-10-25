d7566e791abcb312cac5f7f247635e7df3c73f6f
Diesos <ozturk@live.Fr>
2023-10-25 16:02:39 +0200

    typo

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,6 @@ void print_array(int *a, int n)
 	int i;
 	int j;
 	int size;
-	int index;
 	int range;
 
 	i = 0;
@@ -21,7 +20,6 @@ void print_array(int *a, int n)
 		i++;
 	i--;
 	size = i;
-	index = size - 1;
 	range = size - n;
 	while (range > j)
 	{
