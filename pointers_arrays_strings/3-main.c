84d752dc04549a29a465d83919c98c2f76728b8e
Diesos <ozturk@live.Fr>
2023-10-27 14:30:43 +0200

    exc 3

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,9 +1,28 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
+int main(void)
+{
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
+    return (0);
+}
+
+
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
 int main(void)
 {
     char *str;
@@ -12,3 +31,4 @@ int main(void)
     _puts(str);
     return (0);
 }
+*/
---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcmp - compare s1 & s2 lexicographically
+ * @s1: first source
+ * @s2: second source
+ * Return: void
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (*s1 - *s2);
+}
