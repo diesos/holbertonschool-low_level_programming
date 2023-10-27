6d45f85e718bff336077e4fdf23487107dba7552
Diesos <ozturk@live.Fr>
2023-10-27 13:25:27 +0200

    exc 1

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strncat - resetting a pointer value
+ * @dest: destination
+ * @src: source
+ * @n: int n time to copy
+ * Return: void
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (*src != '\0')
+	{
+		while (n >= j)
+		{
+			dest = src;
+			src++;
+			dest++;
+			j++;
+		}
+		src++;
+	}
+	*dest = '\0';
+	return (dest);
+}
