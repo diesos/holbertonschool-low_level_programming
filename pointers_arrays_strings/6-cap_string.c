5fb78fded14186ba16baafab205998903f0befe7
Diesos <ozturk@live.Fr>
2023-10-29 22:31:12 +0100

    betty style

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,32 +2,36 @@
 #include <stdio.h>
 
 /**
- * cap_String - transform to uppercase every first letter of word
+ * cap_string - transform to uppercase every first letter of word
  * @s: pointor of char string
  *
- * Return: nothing.
+ * Return: s.
  */
 
-char *cap_string(char *s)
+#include <string.h>
+
+char	*cap_string(char *s)
 {
-    int i = 0;
+	int		i;
+	char	*word_boundaries;
 
-    while (s[i] != '\0')
-    {
-        if (s[i] == 9 || s[i] == 32 || s[i] == 44 || (s[i] >= 59 && s[i] <= 63) || s[i] == 123 || s[i] == 125 || (s[i] >= 40 && s[i] <= 41) || (s[i] >= 44 && s[i] <= 46) || (s[i] >= 32 && s[i] <= 34))
-        {
-            i++;
-			if (s[i] == 9 || s[i] == 32 || s[i] == 44 || (s[i] >= 59 && s[i] <= 63) || s[i] == 123 || s[i] == 125 || (s[i] >= 40 && s[i] <= 41) || (s[i] >= 44 && s[i] <= 46) || (s[i] >= 32 && s[i] <= 34))
-			{
-				i++;
-				if (s[i] >= 97 && s[i] <= 122)
-					s[i] = s[i] - 32;
-			}
+	i = 0;
+	if (s[i] >= 97 && s[i] <= 122)
+	{
+		s[i] = s[i] - 32;
+	}
+	word_boundaries = " \t\n,;.!?\"(){}";
+	while (s[i] != '\0')
+	{
+		if (strchr(word_boundaries, s[i]) != NULL)
+		{
+			i++;
 			if (s[i] >= 97 && s[i] <= 122)
+			{
 				s[i] = s[i] - 32;
+			}
 		}
-        i++;
-    }
-
-    return s;
+		i++;
+	}
+	return (s);
 }
