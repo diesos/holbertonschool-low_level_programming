5248cd30813e5341d91e06d4982ea4641a081179
Diesos <ozturk@live.Fr>
2023-10-29 21:41:27 +0100

    betty coding style edit

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,7 +3,7 @@
 
 /**
  * string_toupper - transform lowercase to uppercase
- * @s: pointor of char string 
+ * @s: pointor of char string
  *
  * Return: nothing.
  */
@@ -16,9 +16,7 @@ char *string_toupper(char *s)
 	while (s[i] != '\0')
 	{
 		if (s[i] >= 96 && s[i] <= 123)
-			{
-				s[i] = s[i] - 32;
-			}
+			s[i] = s[i] - 32;
 		i++;
 	}
 	return (s);
