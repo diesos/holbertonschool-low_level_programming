a018be48e72d0105ac4caf538f2d277bf92ac344
Diesos <ozturk@live.Fr>
2023-10-29 21:39:21 +0100

    exc 5

--- pointers_arrays_strings/5-main.c
+++ pointers_arrays_strings/5-main.c
@@ -5,7 +5,6 @@
  * main - check the code
  *
  * Return: Always 0.
- */
 int main(void)
 {
     char s[10] = "My School";
@@ -13,5 +12,22 @@ int main(void)
     printf("%s\n", s);
     rev_string(s);
     printf("%s\n", s);
+    return (0);
+}
+*/
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char str[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(str);
+    printf("%s", ptr);
+    printf("%s", str);
     return (0);
 }
---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - transform lowercase to uppercase
+ * @s: pointor of char string 
+ *
+ * Return: nothing.
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 96 && s[i] <= 123)
+			{
+				s[i] = s[i] - 32;
+			}
+		i++;
+	}
+	return (s);
+}
