ebd7ac4be28391c334b2a01efd7541a26ba03d8f
Diesos <ozturk@live.Fr>
2023-10-30 16:04:06 +0100

    exc 0

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function to puts recursively
+* @s: pointor to be puts recursively
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
