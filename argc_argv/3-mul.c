151878a637f21e359e2226c82a3c836edaa32dcc
Diesos <ozturk@live.Fr>
2023-11-03 23:27:22 +0100

    betty style

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,16 +10,19 @@
 
 int	main(int ac, char *av[])
 {
-    if (ac > 2)
-    {
-        int num1 = atoi(av[1]);
-        int num2 = atoi(av[2]);
-	    printf("%d\n", num1 * num2);
-    }
-    else
-    {
-        printf("Error\n");
-	    return (1);
-    }
-    return (0);
+	int	num1;
+	int	num2;
+
+	if (ac > 2)
+	{
+		num1 = atoi(av[1]);
+		num2 = atoi(av[2]);
+		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
 }
