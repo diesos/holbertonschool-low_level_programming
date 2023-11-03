d70f8179905abf70855963066c7a3fa55dbff5d1
Diesos <ozturk@live.Fr>
2023-11-03 22:56:59 +0100

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: Always 0.
+ */
+
+int	main(int ac, char *av[])
+{
+    if ( ac == 0)
+        printf("0");
+    else
+        printf("%d\n", ac - 1);
+    (void)av;
+	return (0);
+}
