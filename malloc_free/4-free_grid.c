3ae9b9b58486ce995ace93c97c0a9be04aabab17
Diesos <ozturk@live.Fr>
2023-11-10 23:32:50 +0100

    P3 T4

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -1,22 +1,21 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2-dimensional grid
- * @grid: pointer to the 2D array
- * @height: height of the grid
+ * free_grid - frees grid
+ * @grid: pointe to  array
+ * @height: height of grid
  *
  * Return: Nothing.
  */
-void free_grid(int **grid, int height)
+void	free_grid(int **grid, int height)
 {
-	int i;
+	int	i;
 
 	if (grid == NULL || height <= 0)
-        return;
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
-    free(grid);
+		return ;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
 }
-
