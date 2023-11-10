267ace847588ff135ead1c3e69ac1e08ad2f4efe
Diesos <ozturk@live.Fr>
2023-11-10 10:29:21 +0100

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* _calloc - allocate mem for array
+* @nmemb: of nmemb
+* @size: of size byte
+* Return: void
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	void *tmp;
+
+	for ( i = 0; i < nmemb; i++)
+	{
+		tmp = malloc((size) * (1));
+		tmp = (char*)tmp + 1;
+	}
+	return (tmp);
+}
