f5fb25805acca773465abb8b656f4c9c060a88e6
Diesos <ozturk@live.Fr>
2023-11-11 00:00:56 +0100

    multiply function

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - Multiplies two integers.
+ * @a: The first integer.
+ * @b: The second integer.
+ *
+ * Return: The result of the multiplication.
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
