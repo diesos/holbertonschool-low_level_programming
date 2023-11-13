9b45fa8b197b4adfbba94a2455a8a1e0368412b6
Diesos <ozturk@live.Fr>
2023-11-13 17:42:31 +0100

    typedef struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,14 +13,14 @@
  *
  * This structure help identify dog name, age & owner
  */
-struct		dog
+struct				dog
 {
-	char	*name;
-	float	age;
-	char	*owner;
+	char			*name;
+	float			age;
+	char			*owner;
 };
-
-void		init_dog(struct dog *d, char *name, float age, char *owner);
-void		print_dog(struct dog *d);
+typedef struct dog	dog_t;
+void				init_dog(struct dog *d, char *name, float age, char *owner);
+void				print_dog(struct dog *d);
 
 #endif
