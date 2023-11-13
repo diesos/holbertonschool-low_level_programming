ff689a27f478514afc1968a2862a4e0cfc0ac301
Diesos <ozturk@live.Fr>
2023-11-14 00:22:41 +0100

    betty style

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,12 @@
 #include "function_pointers.h"
 
+/**
+ * print_name - check if nothing is null
+ * @name: name of dog
+ * @f: function to be used
+ * Return: void.
+ */
+
 void	print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
