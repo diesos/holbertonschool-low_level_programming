3c592b782331e52020d078f79b27f97d212a3bb2
Diesos <ozturk@live.Fr>
2023-11-13 15:51:00 +0100

    description of struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,15 +1,23 @@
 #ifndef DOG_H
-#define DOG_H
+# define DOG_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-struct dog
+/**
+ * struct dog - Dog structure
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * This structure help identify dog name, age & owner
+ */
+struct		dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char	*name;
+	float	age;
+	char	*owner;
 };
 
 #endif
