c1be4c072cdfad59e8753b6a5382a5bc245e555d
Diesos <ozturk@live.Fr>
2023-11-13 16:17:56 +0100

    function to initialize struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog- initialize dog struct
+ * @d: pointor to initilize struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: void.
+ */
+
+void	init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
