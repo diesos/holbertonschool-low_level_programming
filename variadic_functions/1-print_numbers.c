ca59267a7126ec179d83730a5fcd17270e486186
Diesos <ozturk@live.Fr>
2023-11-15 15:12:38 +0100

    variadic to print integers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints numbers followed by a separator
+ * @separator: String to be printed between numbers
+ * @n: Number of parameters
+ * @...: Variable number of parameters (integers) to be printed
+ */
+
+void	print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list			args;
+	unsigned int	sum;
+	int				total;
+	unsigned int	i;
+
+	va_start(args, n);
+	total = 0;
+	for (i = 0; i < n; i++)
+	{
+		if (separator == NULL)
+			exit(0);
+		sum = va_arg(args, unsigned int);
+		printf("%d", sum);
+		if (i != (n - 1))
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
-#define MAIN_H
+# define MAIN_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+# include <stdarg.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int sum_them_all(const unsigned int n, ...);
+int		sum_them_all(const unsigned int n, ...);
+void	print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
