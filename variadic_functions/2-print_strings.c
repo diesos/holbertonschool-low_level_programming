80563e6ebcc9db16351ebb1d3fe0609b65f923b7
Diesos <ozturk@live.Fr>
2023-11-15 15:39:01 +0100

    variadic function to print strings

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - Prints strings followed by a separator
+ * @separator: String to be printed between strings
+ * @n: Number of parameters
+ * @...: Variable number of parameters (strings) to be printed
+ */
+
+void	print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list			args;
+	char			*sum;
+	unsigned int	i;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		sum = va_arg(args, char *);
+		if (sum[0] == '\0')
+			printf("(nil)");
+		printf("%s", sum);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(args);
+}
