fb9df944215ad5d55306d441ddd4a41f11734551
Diesos <ozturk@live.Fr>
2023-11-29 15:44:24 +0100

    Corrected for betty style

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
 #ifndef LISTS_H
-#define LISTS_H
+# define LISTS_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,12 +15,11 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
-} list_t;
+	char			*str;
+	unsigned int	len;
+	struct list_s	*next;
+}					list_t;
 
-
-size_t print_list(const list_t *h);
+size_t				print_list(const list_t *h);
 
 #endif
