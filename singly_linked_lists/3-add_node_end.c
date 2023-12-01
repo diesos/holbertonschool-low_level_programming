671df6e77aee3a068cb70be1664e599beef88dcb
Diesos <ozturk@live.Fr>
2023-12-01 15:58:31 +0100

    Betty coding style purpose modification

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -7,29 +7,29 @@
  * Return: adress of new node
  */
 
-list_t *add_node_end(list_t **head, const char *str)
+list_t	*add_node_end(list_t **head, const char *str)
 {
-    list_t *new;
-    list_t *temp;
+	list_t	*new;
+	list_t	*temp;
 
-    new = malloc(sizeof(list_t));
-    new->str = strdup(str);
-    new->len = strlen(new->str);
-    if (*head == NULL)
-    {
-        *head = new;
-        /* On crée le premier noeud. Si head n'a pas de next*/
-    }
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-        {
-            temp = temp->next;
-        }
-        temp->next = new;
-    }
-    if (!(new))
-        return (NULL);
-    return (new);
+	new = malloc(sizeof(list_t));
+	new->str = strdup(str);
+	new->len = strlen(new->str);
+	if (*head == NULL)
+	{
+		*head = new;
+		/* On crée le premier noeud. Si head n'a pas de next*/
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	if (!(new))
+		return (NULL);
+	return (new);
 }
