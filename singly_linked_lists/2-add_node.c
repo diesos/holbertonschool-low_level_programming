0231255b959d34d3dbb33f2d2f8413743ccada28
Diesos <ozturk@live.Fr>
2023-12-01 15:23:17 +0100

    free malloc while unused

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,26 +7,27 @@
  * Return: adress of new node
  */
 
-list_t	*add_node(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
-	list_t	*new;
+    list_t *new;
 
-	new = malloc(sizeof(list_t));
-	new->next = *head;
-	new->str = strdup(str);
-	new->len = strlen(new->str);
-	*head = new;
-	if (new == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	if (!(new->str))
-	{
-		free(new);
-		return (NULL);
-	}
-	if (!(new))
-		return (NULL);
-	return (new);
+    new = malloc(sizeof(list_t));
+    new->next = *head;
+    new->str = strdup(str);
+    new->len = strlen(new->str);
+    *head = new;
+    if (new == NULL)
+    {
+        free(new);
+        return (NULL);
+    }
+    if (!(new->str))
+    {
+        free(new);
+        return (NULL);
+    }
+    if (!(new))
+        return (NULL);
+    free(new);
+    return (new);
 }
