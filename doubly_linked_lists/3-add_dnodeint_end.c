c12d03b6abba41ad8da7d92b55cf327c0b6e6475
Diesos <ozturk@live.Fr>
2023-12-01 22:55:33 +0100

    handling errors and improved code

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,30 +7,29 @@
  * Return: Pointer to the newly added node, or NULL on failure.
  */
 
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+dlistint_t	*add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *newNode;
-    dlistint_t *temp;
+	dlistint_t	*newNode;
+		dlistint_t *temp;
 
-    newNode = malloc(sizeof(dlistint_t));
-    temp = malloc(sizeof(dlistint_t));
-    newNode->n = n;
-
-    if (newNode == NULL)
-    {
-        free(newNode);
-        return (NULL);
-    }
-    newNode->prev = temp;
-    newNode->next = NULL;
-    if (*head == NULL)
-        *head = newNode;
-    else
-    {
-        temp = *head;
-        while (temp->next != NULL)
-            temp = temp->next;
-        temp->next = newNode;
-    }
-    return (newNode);
+	newNode = malloc(sizeof(dlistint_t));
+	newNode->n = n;
+	newNode->next = NULL;
+	if (newNode == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		newNode->prev = NULL;
+		*head = newNode;
+	}
+	else
+	{
+		temp = malloc(sizeof(dlistint_t));
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = newNode;
+		newNode->prev = temp;
+	}
+	return (newNode);
 }
