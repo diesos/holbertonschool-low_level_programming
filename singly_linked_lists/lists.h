34c8f331feeb641b3493ce3f2c193e4286e2ba2c
Diesos <ozturk@live.Fr>
2023-12-01 15:16:03 +0100

    header file updated

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -1,10 +1,10 @@
 #ifndef LISTS_H
-# define LISTS_H
+#define LISTS_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <string.h>
-# include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -15,11 +15,14 @@
  */
 typedef struct list_s
 {
-	char			*str;
-	unsigned int	len;
-	struct list_s	*next;
-}					list_t;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
 
-size_t				print_list(const list_t *h);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 
 #endif
