7975da8864f2a142d0f7e2d4e3c457025597676f
Diesos <ozturk@live.Fr>
2023-12-01 15:26:56 +0000

    function to free a list_t

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - function to free a list_t
+ * @head: ptr to head
+ * @str: string to add to the node
+ * Return: adress of new node
+ */
+
+ void free_list(list_t *head)
+{
+	while (head != NULL)
+    {
+       free(head->str);
+       free(head);
+       head = head->next;
+    }
+}
