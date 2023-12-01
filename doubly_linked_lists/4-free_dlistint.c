ffd55d8ea7f927d3c78c5f06b79ddacf9f84ede5
Diesos <ozturk@live.Fr>
2023-12-01 23:14:40 +0100

    function to free dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_dlist - free memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+void	free_dlistint(dlistint_t *head)
+{
+	dlistint_t	*temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
