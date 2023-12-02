80f161ff575bd567b0a93aba87a78ae2a41b66b3
Diesos <ozturk@live.Fr>
2023-12-02 16:16:48 +0100

    function to sum all nodes data

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - Computes the sum of all integers in a doubly linked list.
+ *
+ * @head: Pointer to the head of the doubly linked list.
+ *
+ * Return: Sum of all integers in the list, or 0 if the list is empty.
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+    int count = 0;
+
+    if (head)
+    {
+        while (head)
+        {
+            if (head->n == NULL)
+                head = head->next;
+            count = count + (head->n); /* On compte count à chaque boucle*/
+            head = head->next;         /* On vas naviguer entre les listes chaînée*/
+        }
+    }
+    return (count);
+}
