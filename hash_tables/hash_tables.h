0ae41a1559182cde54678e97d375003f95c40025
Diesos <ozturk@live.Fr>
2023-12-08 14:55:28 +0100

    betty style compliance

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -2,13 +2,13 @@
 # define HASH_TABLES_H
 
 # include <fcntl.h>
+# include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <sys/stat.h>
 # include <sys/types.h>
 # include <unistd.h>
-#include <stdint.h>
 
 /**
  * struct hash_node_s - Node of a hash table
@@ -20,10 +20,10 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
-} hash_node_t;
+	char				*key;
+	char				*value;
+	struct hash_node_s	*next;
+}						hash_node_t;
 
 /**
  * struct hash_table_s - Hash table data structure
@@ -35,17 +35,19 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
-} hash_table_t;
+	unsigned long int	size;
+	hash_node_t			**array;
+}						hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
+hash_table_t			*hash_table_create(unsigned long int size);
+unsigned long int		hash_djb2(const unsigned char *str);
+unsigned long int		key_index(const unsigned char *key,
+							unsigned long int size);
+int						hash_table_set(hash_table_t *ht, const char *key,
+							const char *value);
+char					*hash_table_get(const hash_table_t *ht,
+							const char *key);
+void					hash_table_print(const hash_table_t *ht);
+void					hash_table_delete(hash_table_t *ht);
 
 #endif
