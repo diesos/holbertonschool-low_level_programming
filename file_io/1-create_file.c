4ee2e4f67484a62d88f5757f4fe781bd427437f9
Diesos <ozturk@live.Fr>
2023-12-08 14:20:34 +0100

    Create a file with filename and write text inside

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,21 +1,19 @@
 #include "main.h"
 
 /**
- * create_file - creates file with the filename and writes text inside
- * @filename: name of the file to be created
- * @text_content: content to be written to the file
+ * create_file - Creates a file with filename and writes text content.
+ * @filename: Name of the file to be created.
+ * @text_content: Content to be written to the file.
  *
  * Return: On success, returns 1. On failure, returns -1.
  */
-
 int	create_file(const char *filename, char *text_content)
 {
-	int	fd;
-	int	n;
+	int fd, n;
 
-	if (filename == NULL)
+	if (!filename || !text_content)
 		return (-1);
-	fd = open(filename, (O_CREAT | O_RDWR | O_APPEND), 0600);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 	n = strlen(text_content);
@@ -24,5 +22,6 @@ int	create_file(const char *filename, char *text_content)
 		close(fd);
 		return (-1);
 	}
+	close(fd);
 	return (1);
 }
