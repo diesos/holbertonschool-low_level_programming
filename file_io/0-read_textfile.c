dd6cb8c408dca46e465dea25dc4f1f6e95ef839e
Diesos <ozturk@live.Fr>
2023-12-08 14:18:23 +0100

    read a text file and prints it in POSIX standart

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,34 +1,42 @@
 #include "main.h"
 
 /**
- * read_textfile - reads a text file and prints it to the posix standard output
- * @filename: a character
- * @letters: len
+ * read_textfile - Reads a text file and prints it.
+ * @filename: Name of the file to read.
+ * @letters: Number of bytes to read and print.
  *
- * Return: writeFile
+ * Return: Number of bytes read and printed. Returns 0 on failure.
  */
-
 ssize_t	read_textfile(const char *filename, size_t letters)
 {
 	int		fd;
-	int		n;
+	ssize_t	n;
 	char	*buff;
 
-	n = 0;
 	if (filename == NULL)
 		return (0);
 	buff = malloc(sizeof(char) * letters);
-	if (buff)
+	if (buff == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 	{
-		fd = open(filename, O_RDONLY);
-        if (filename == NULL)
-            return (0);
-		n = read(fd, buff, letters);
-		write(1, buff, n);
+		free(buff);
+		return (0);
+	}
+	n = read(fd, buff, letters);
+	if (n == -1)
+	{
 		close(fd);
 		free(buff);
-		return (n);
-	}
-	else
 		return (0);
+	}
+	write(1, buff, n);
+	if (close(fd) == -1)
+	{
+		free(buff);
+		return (0);
+	}
+	free(buff);
+	return (n);
 }
