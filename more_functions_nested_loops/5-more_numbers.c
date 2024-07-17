02746897af5fac608abcc56e06db53ae505046c8
Diesos <ozturk@live.Fr>
2024-07-17 12:44:15 +0200

    Refactor more_numbers function for improved readability and remove unnecessary code

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,24 +1,22 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
  * more_numbers - prints 10 times the numbers, from 0 to 14
  * Return: Always 0
  */
-void more_numbers(void)
+void	more_numbers(void)
 {
 	int i, num;
-
 	for (i = 0; i < 10; i++)
 	{
-
 		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
 			{
-				_putchar(num / 10 + '0');
+				putchar(num / 10 + '0');
 			}
-			_putchar(num % 10 + '0');
+			putchar(num % 10 + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
